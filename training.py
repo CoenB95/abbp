a33aa23852c570c828008a924c460d66868e8fa4
kjwdamme <kjwdamme@avans.nl>
2019-11-18 11:05:54 +0100

    nog een fix in parallel_model.py

--- training.py
+++ training.py
@@ -34,7 +34,7 @@ class ABBPConfig(Config):
     NAME = "ABBP"
 
     # Adjust down if you use a smaller GPU.
-    IMAGES_PER_GPU = 1
+    IMAGES_PER_GPU = 2
 
     # Number of classes (including background)
     NUM_CLASSES = 1 + 6  # Background + balloon
@@ -43,6 +43,9 @@ class ABBPConfig(Config):
 
     # Skip detections with < 90% confidence
     DETECTION_MIN_CONFIDENCE = 0.9
+
+    # NUMBER OF GPUs to use. When using only a CPU, this needs to be set to 1.
+    GPU_COUNT = 2
 
 
 # Dataset class ABBPDataset(utils.Dataset):
@@ -135,108 +138,112 @@ def train(model):
                 layers='heads')
 
 
-def tests():
-    dataset = ABBPDataset()
-    dataset.load_object("images")
-
-    info = dataset.image_info[0]
-
-    print(info['id'])
-    mask, class_ids = dataset.load_mask(5)
-
-    _idx = np.sum(mask, axis=(0, 1)) > 0
-    mask = mask[:, :, _idx]
-    class_ids = class_ids[_idx]
-
-    cv2.imshow('mask', np.array(mask * 255, dtype=np.uint8))
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-def main():
-    import argparse
-
-    # Parse command line arguments
-    parser = argparse.ArgumentParser(
-        description='Train Mask R-CNN to detect objects.')
-    parser.add_argument("command",
-                        metavar="<command>",
-                        help="'train' or 'splash'")
-    parser.add_argument('--dataset', required=False,
-                        metavar="/path/to/balloon/dataset/",
-                        help='Directory of the Balloon dataset')
-    parser.add_argument('--weights', required=True,
-                        metavar="/path/to/weights.h5",
-                        help="Path to weights .h5 file or 'coco'")
-    parser.add_argument('--logs', required=False,
-                        default=DEFAULT_LOGS_DIR,
-                        metavar="/path/to/logs/",
-                        help='Logs and checkpoints directory (default=logs/)')
-    parser.add_argument('--image', required=False,
-                        metavar="path or URL to image",
-                        help='Image to apply the color splash effect on')
-    parser.add_argument('--video', required=False,
-                        metavar="path or URL to video",
-                        help='Video to apply the color splash effect on')
-    args = parser.parse_args()
-
-    # Validate arguments
-    if args.command == "train":
-        assert args.dataset, "Argument --dataset is required for training"
-    elif args.command == "splash":
-        assert args.image or args.video, \
-            "Provide --image or --video to apply color splash"
-
-    print("Weights: ", args.weights)
-    print("Dataset: ", args.dataset)
-    print("Logs: ", args.logs)
-
-    # Configurations
-    if args.command == "train":
-        config = ABBPConfig()
-    else:
-        class InferenceConfig(ABBPConfig):
-            # Set batch size to 1 since we'll be running inference on
-            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
-            GPU_COUNT = 1
-            IMAGES_PER_GPU = 1
-
-        config = InferenceConfig()
-    config.display()
-
-    # Create model
-    if args.command == "train":
-        model = modellib.MaskRCNN(mode="training", config=config,
-                                  model_dir=args.logs)
-    else:
-        model = modellib.MaskRCNN(mode="inference", config=config,
-                                  model_dir=args.logs)
-
-    # Select weights file to load
-    if args.weights.lower() == "coco":
-        weights_path = COCO_WEIGHTS_PATH
-        # Download weights file
-        if not os.path.exists(weights_path):
-            utils.download_trained_weights(weights_path)
-    elif args.weights.lower() == "last":
-        # Find last trained weights
-        weights_path = model.find_last()
-    elif args.weights.lower() == "imagenet":
-        # Start from ImageNet trained weights
-        weights_path = model.get_imagenet_weights()
-    else:
-        weights_path = args.weights
-
-    # Load weights
-    print("Loading weights ", weights_path)
-
-    # number of classes
-    model.load_weights(weights_path, by_name=True, exclude=[
-        "mrcnn_class_logits", "mrcnn_bbox_fc",
-        "mrcnn_bbox", "mrcnn_mask"])
-    # Train or evaluate
-    train(model)
-
-
-main()
+# def tests():
+#     dataset = ABBPDataset()
+#     dataset.load_object("images")
+#
+#     info = dataset.image_info[0]
+#
+#     print(info['id'])
+#     mask, class_ids = dataset.load_mask(5)
+#
+#     _idx = np.sum(mask, axis=(0, 1)) > 0
+#     mask = mask[:, :, _idx]
+#     class_ids = class_ids[_idx]
+#
+#     cv2.imshow('mask', np.array(mask * 255, dtype=np.uint8))
+#
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+#
+#
+# def main():
+#     import tensorflow as tf
+#     print("doe het")
+#     print("TEST: " + str(tf.test.gpu_device_name()))
+#
+#     import argparse
+#
+#     # Parse command line arguments
+#     parser = argparse.ArgumentParser(
+#         description='Train Mask R-CNN to detect objects.')
+#     parser.add_argument("command",
+#                         metavar="<command>",
+#                         help="'train' or 'splash'")
+#     parser.add_argument('--dataset', required=False,
+#                         metavar="/path/to/balloon/dataset/",
+#                         help='Directory of the Balloon dataset')
+#     parser.add_argument('--weights', required=True,
+#                         metavar="/path/to/weights.h5",
+#                         help="Path to weights .h5 file or 'coco'")
+#     parser.add_argument('--logs', required=False,
+#                         default=DEFAULT_LOGS_DIR,
+#                         metavar="/path/to/logs/",
+#                         help='Logs and checkpoints directory (default=logs/)')
+#     parser.add_argument('--image', required=False,
+#                         metavar="path or URL to image",
+#                         help='Image to apply the color splash effect on')
+#     parser.add_argument('--video', required=False,
+#                         metavar="path or URL to video",
+#                         help='Video to apply the color splash effect on')
+#     args = parser.parse_args()
+#
+#     # Validate arguments
+#     if args.command == "train":
+#         assert args.dataset, "Argument --dataset is required for training"
+#     elif args.command == "splash":
+#         assert args.image or args.video, \
+#             "Provide --image or --video to apply color splash"
+#
+#     print("Weights: ", args.weights)
+#     print("Dataset: ", args.dataset)
+#     print("Logs: ", args.logs)
+#
+#     # Configurations
+#     if args.command == "train":
+#         config = ABBPConfig()
+#     else:
+#         class InferenceConfig(ABBPConfig):
+#             # Set batch size to 1 since we'll be running inference on
+#             # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+#             GPU_COUNT = 1
+#             IMAGES_PER_GPU = 1
+#
+#         config = InferenceConfig()
+#     config.display()
+#
+#     # Create model
+#     if args.command == "train":
+#         model = modellib.MaskRCNN(mode="training", config=config,
+#                                   model_dir=args.logs)
+#     else:
+#         model = modellib.MaskRCNN(mode="inference", config=config,
+#                                   model_dir=args.logs)
+#
+#     # Select weights file to load
+#     if args.weights.lower() == "coco":
+#         weights_path = COCO_WEIGHTS_PATH
+#         # Download weights file
+#         if not os.path.exists(weights_path):
+#             utils.download_trained_weights(weights_path)
+#     elif args.weights.lower() == "last":
+#         # Find last trained weights
+#         weights_path = model.find_last()
+#     elif args.weights.lower() == "imagenet":
+#         # Start from ImageNet trained weights
+#         weights_path = model.get_imagenet_weights()
+#     else:
+#         weights_path = args.weights
+#
+#     # Load weights
+#     print("Loading weights ", weights_path)
+#
+#     # number of classes
+#     model.load_weights(weights_path, by_name=True, exclude=[
+#         "mrcnn_class_logits", "mrcnn_bbox_fc",
+#         "mrcnn_bbox", "mrcnn_mask"])
+#     # Train or evaluate
+#     train(model)
+#
+#
+# main()
