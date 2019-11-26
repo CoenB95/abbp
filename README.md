6da1b2660d084816c7d40d3897a9f05487307db5
kjwdamme <kjwdamme@avans.nl>
2019-11-26 15:53:35 +0100

    weer al

---
+++ README.md
@@ -0,0 +1,61 @@
+# ABBP Installation
+Optional (to make life easy):  
+`export ROS_WS=$HOME/catkin_ws`  
+`export UBUNTU_DISTRO=bionic`  
+
+## ROS
+- To install version 'melodic':  
+`sudo apt install ros-melodic-desktop-full`  
+- Setup catkin workspace:  
+`mkdir $ROS_WS`  
+`mkdir $ROS_WS/src`  
+**`cd $ROS_WS/src`**  
+`catkin_init_workspace`  
+**`cd $ROS_WS`**  
+`catkin_make`  
+- Source created setup.bash: (restart terminal to apply)  
+**`cd $HOME`**  
+`gedit .bashrc`  
+add line `source $ROS_WS/devel/setup.bash`  
+## Camera
+- Install Realsense Driver:  
+`sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE`  
+`sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo $ main" $UBUNTU_DISTRO -u`  
+`sudo apt-get install librealsense2-dkms`  
+`sudo apt-get install librealsense2-utils`  
+- Install Realsense ROS-package:  
+**`cd $ROS_WS/src`**  
+`git clone https://github.com/pal-robotics/ddynamic_reconfigure.git -b kinetic-devel`  
+`git clone https://github.com/IntelRealSense/realsense-ros.git`  
+**`cd $ROS_WS`**  
+`catkin_make install`  
+## Vision
+**Requirements:**
+- Python >3.6.9
+- Tensorflow =1.15.0
+- Keras =2.2.5
+### Windows
+- Install shapely via conda because PyPi does not have shapely:  
+`conda install -c conda-forge shapely`  
+- Install all dependencies:  
+`pip install -r requirements.txt`  
+- Download [Intel Realsense drivers](https://downloadcenter.intel.com/product/128256/Intel-RealSense-Depth-Camera-D415)  
+### UNIX / Linux
+- Install shapely via conda because PyPi does not have shapely:  
+`conda install -c conda-forge shapely`  
+- Install all dependencies:  
+`pip install -r requirements.txt`  
+- The pyrealsense2 library needs to built with the source files:  
+  `git clone https://github.com/IntelRealSense/librealsense`  
+  `cd librealsense`  
+  `mkdir build`  
+  `cd build`  
+  ###### Note: To force compilation with a specific version on a system with both Python 2 and Python 3 installed, add the following flag to CMake command: -DPYTHON_EXECUTABLE=[full path to the exact python executable]  
+  `cmake ../ -DBUILD_PYTHON_BINDINGS=TRUE`  
+  `make -j4`  
+  `sudo make install #Optional if you want the library to be installed in your system`  
+  If it all goes without errors, you should be able to find the pyrealsense2.<arch info>.so under build/wrappers/python (actually 3 files with the same name and extensions .so, .so.2, .so.2.8.1). Now the easiest way to use it is run python from that folder and import pyrealsense2 or extract the .so files to the root of your files.  
+
+## ABBP
+**`cd $ROS_WS/src`**  
+`git clone https://github.com/telbir/abbp.git`  
---
+++ robot/src/abbp_robot_node.cpp
@@ -0,0 +1,112 @@
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
+#include "abbp_robot_node.h"
+
+namespace rvt = rviz_visual_tools;
+
+int main(int argc, char** argv) {
+  init(argc, argv, "abbp_robot_node");
+  AsyncSpinner spinner(1);
+  spinner.start();
+  Roscovery roscovery;
+}
+
+const vector<string> Roscovery::JOINT_NAMES = {"shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint", "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"};
+const std::string Roscovery::PLANNING_GROUP = "manipulator";
+
+Roscovery::Roscovery() {
+  ROS_INFO("ABBP Robot started");
+
+  moveGroup = new MoveGroupInterface(PLANNING_GROUP);
+  planningSceneInterface = new PlanningSceneInterface();
+  visualTools = new MoveItVisualTools("world");
+
+  visualTools->deleteAllMarkers();
+  visualTools->loadRemoteControl();
+
+  Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
+  text_pose.translation().z() = 1.75;
+  visualTools->publishText(text_pose, "MoveGroupInterface Demo", rvt::WHITE, rvt::XLARGE);
+  visualTools->trigger();
+
+  // Getting Basic Information
+  ROS_INFO_STREAM("Planning frame: " << moveGroup->getPlanningFrame());
+  ROS_INFO_STREAM("End effector link: " << moveGroup->getEndEffectorLink());
+  ROS_INFO("Available Planning Groups:");
+  std::copy(moveGroup->getJointModelGroupNames().begin(), moveGroup->getJointModelGroupNames().end(),
+            std::ostream_iterator<std::string>(std::cout, ", "));
+
+  visualTools->prompt("Press 'next' in the RvizVisualToolsGui window to start the demo");
+
+  addFloor();
+
+  geometry_msgs::Pose target_pose1;
+  target_pose1.position.x = -0.614;
+  target_pose1.position.y = 0.051;
+  target_pose1.position.z = 0.250;
+
+  tf2::Quaternion quat;
+  quat.setEuler(M_PI, 0, 0);
+  tf2::convert(quat, target_pose1.orientation);
+
+  moveGroup->setPoseTarget(target_pose1);
+  moveGroup->setMaxVelocityScalingFactor(0.1);
+  moveGroup->setNumPlanningAttempts(5);
+
+  ROS_INFO("Visualizing target pose");
+  visualTools->deleteAllMarkers();
+  visualTools->publishAxisLabeled(target_pose1, "Target");
+  visualTools->trigger();
+  visualTools->prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
+
+  ROS_INFO("Planning movement...");
+  MoveGroupInterface::Plan my_plan;
+  MoveItErrorCode planResult = moveGroup->plan(my_plan);
+  ROS_INFO("Planning %s", planResult == MoveItErrorCode::SUCCESS ? "SUCCEEDED" : "FAILED");
+
+  ROS_INFO("Visualizing plan as trajectory line");
+  visualTools->publishTrajectoryLine(my_plan.trajectory_, moveGroup->getCurrentState()->getJointModelGroup(PLANNING_GROUP));
+  visualTools->trigger();
+  visualTools->prompt("Press 'next' in the RvizVisualToolsGui window to continue the demo");
+
+  ROS_INFO("Moving robot according to plan...");
+  MoveItErrorCode executionResult = moveGroup->execute(my_plan);
+  ROS_INFO("Execution %s", planResult == MoveItErrorCode::SUCCESS ? "SUCCEEDED" : "FAILED");
+}
+
+void Roscovery::addFloor() {
+  // Define a box to add to the world.
+  shape_msgs::SolidPrimitive objectShape;
+  objectShape.type = shape_msgs::SolidPrimitive::BOX;
+  objectShape.dimensions.resize(3);
+  objectShape.dimensions[0] = 2.0;
+  objectShape.dimensions[1] = 2.0;
+  objectShape.dimensions[2] = 0.1;
+
+  // Define a pose for the box (specified relative to frame_id)
+  geometry_msgs::Pose objectPose;
+  objectPose.orientation.w = 1.0;
+  objectPose.position.x = 0.0;
+  objectPose.position.y = 0.0;
+  objectPose.position.z = -0.05;
+
+  // Define a collision object ROS message.
+  moveit_msgs::CollisionObject collisionObject;
+  collisionObject.header.frame_id = moveGroup->getPlanningFrame();
+  collisionObject.id = "box1";
+  collisionObject.primitives.push_back(objectShape);
+  collisionObject.primitive_poses.push_back(objectPose);
+  collisionObject.operation = collisionObject.ADD;
+
+  std::vector<moveit_msgs::CollisionObject> collision_objects;
+  collision_objects.push_back(collisionObject);
+
+  // Now, let's add the collision object into the world
+  ROS_INFO_NAMED("tutorial", "Add an object into the world");
+  planningSceneInterface->addCollisionObjects(collision_objects);
+
+  // Show text in RViz of status
+  visualTools->trigger();
+  visualTools->prompt("Press 'next' in the RvizVisualToolsGui window to once the collision object appears in RViz");
+}
