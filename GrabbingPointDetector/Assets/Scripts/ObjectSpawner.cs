42e4f516319636ecdbb0e3c6ce37e45bb5426d5d
kjwdamme <kjwdamme@avans.nl>
2019-11-26 15:47:48 +0100

    There is now a bin with randomly spawning objects

---
+++ GrabbingPointDetector/Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject[] objects;
+    public int amountPerObject;
+
+    void Start() 
+    {
+        float maxX = transform.parent.localScale.x;
+        float minX = -transform.parent.localScale.x;
+        float maxZ = transform.parent.localScale.z;
+        float minZ = -transform.parent.localScale.z;
+
+        for (int j = 0; j < amountPerObject; j++) {
+            for(int i = 0; i < objects.Length; i++) {
+                float x = Random.Range(minX, maxX);
+                float z = Random.Range(minZ, maxZ);
+                Debug.Log(x + ", " + z);
+                Instantiate(objects[i], new Vector3(x, transform.position.y, z), Random.rotation);
+            }
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
