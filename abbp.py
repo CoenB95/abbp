8d0b4b6107cf64b7f8d356f7e4ab0da122858851
kjwdamme <kjwdamme@avans.nl>
2019-12-05 11:09:40 +0100

    Removed unnecessary functions

--- abbp.py
+++ abbp.py
@@ -8,6 +8,7 @@ sys.path.append(PYREALSENSE_DIR)
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+import skimage.io
 
 import os
 import tensorflow as tf
@@ -35,6 +36,8 @@ class InferenceConfig(config.__class__):
     # Run detection on one image at a time
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
+    IMAGE_CHANNEL_COUNT = 4
+    MEAN_PIXEL = np.array([123.7, 116.8, 103.9, 0.5])
 
 
 config = InferenceConfig()
@@ -46,23 +49,11 @@ config.display()
 # GPU for training.
 DEVICE = "/cpu:0"  # /cpu:0 or /gpu:0
 
+#with tf.device(DEVICE):
+#     model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR,
+#                              config=config)
 
-def get_ax(rows=1, cols=1, size=16):
-    """Return a Matplotlib Axes array to be used in
-    all visualizations in the notebook. Provide a
-    central point to control graph sizes.
-
-    Adjust the size attribute to control how big to render images
-    """
-    _, ax = plt.subplots(rows, cols, figsize=(size * cols, size * rows))
-    return ax
-
-
-with tf.device(DEVICE):
-    model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR,
-                              config=config)
-
-model.load_weights(OBJECT_WEIGHTS_PATH, by_name=True)
+# model.load_weights(OBJECT_WEIGHTS_PATH, by_name=True)
 
 class_names = [
     "BG",
@@ -144,37 +135,53 @@ def main():
     pipeline = rs.pipeline()
     config = rs.config()
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
     #
     # # Start streaming
     pipeline.start(config)
     #
-    time.sleep(5)
-
 
     while True:
         # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
         color_frame = frames.get_color_frame()
-        if not color_frame:
+        depth_frame = frames.get_depth_frame()
+        if not color_frame or not depth_frame:
             continue
 
-        # Convert images to numpy arrays
         color_image = np.asanyarray(color_frame.get_data())
 
-        results = model.detect([color_image], verbose=1)
+        colorizer = rs.colorizer()
+        colorizer.set_option(rs.option.color_scheme, 2)
+        colorized_depth = np.asanyarray(colorizer.colorize(depth_frame).get_data())
+
+        align = rs.align(rs.stream.color)
+        frameset = align.process(frames)
+
+        aligned_depth_frame = frameset.get_depth_frame()
+        colorized_depth = np.asanyarray(colorizer.colorize(aligned_depth_frame).get_data())
+        converted_depth = skimage.color.rgb2gray(colorized_depth)
+        stacked = np.dstack((color_image, colorized_depth))
+
+        print(stacked.shape)
+
+        # images = np.hstack((stacked[:, :, :3], colorized_depth[:, :, 3]))
+
+        #results = model.detect([color_image], verbose=1)
 
         # Display results
-        ax = get_ax(1)
-        r = results[0]
-        print(r['class_ids'])
-        image = visualize_mask(color_image, r['rois'], r['masks'], r['class_ids'],
-                                        class_names, r['scores'], title="Predictions")
+        #ax = get_ax(1)
+        #r = results[0]
+        #print(r['class_ids'])
+        #image = visualize_mask(color_image, r['rois'], r['masks'], r['class_ids'],
+        #                                class_names, r['scores'], title="Predictions")
 
-        images = np.hstack((color_image, image))
+        # images = np.hstack((converted_depth, colorized_depth))
 
         # Show images
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
+        cv2.imshow('converted', converted_depth)
+        cv2.imshow('Not converted', colorized_depth)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
